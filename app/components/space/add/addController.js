1f9a53edaeacc480a86697c4133575f2db7c1590
yoobin <yoobin@ttadag.com>
2016-01-19 16:31:04 +0900

    isAuthorize 플로우 추가

--- app/components/space/add/addController.js
+++ app/components/space/add/addController.js
@@ -7,8 +7,35 @@
 //ngTtadagApp.space.add.addSpaceController
 
 	angular.module('ngTtadagApp.space.add.controller')
-		.controller('addController', ['$scope', function($scope) {
-			alert('add');
+		.controller('addController', ['$scope', '$location', 'AccountService', '$timeout', function($scope, $location, AccountService, $timeout) {
+
+			$scope.count = 30;
+			
+
+			var buttonCount = function() {
+
+				if ($scope.count >= 1) {
+					$scope.count--;
+					$timeout(buttonCount, 1000);
+				} else {
+					$timeout.cancel(buttonCount);
+					//재인증 페이지로 이동
+					//$location.path('/account/signIn');
+				}
+			};
+
+
+			if (!AccountService.getCookiesInfoIsLogin()) {
+				$location.path('/account/signIn');
+			} else {
+
+
+				$timeout(buttonCount, 1000);
+			}
+
+
+			console.log('!!!!!');
+
 		}]);
 
 })();
