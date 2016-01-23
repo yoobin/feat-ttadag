f415ff94ed55b4c5039374b6b7ede6866909b14a
yoobin <yoobin@ttadag.com>
2016-01-23 19:17:58 +0900

    taskEdit tand by commit 2

--- app/components/space/taskEdit/taskEditController.js
+++ app/components/space/taskEdit/taskEditController.js
@@ -1,7 +1,25 @@
 (function() {
 	angular.module('ngTtadagApp.spaceEditList.taskEditController')
-		.controller('taskEditController', ['$scope', '$routeParams', function($scope, $routeParams) {
-			console.log($routeParams.id);
-			console.log($scope)
+		.controller('taskEditController', ['$scope', '$routeParams', '$http', 'AccountService', function($scope, $routeParams, $http, AccountService) {
+			$scope.index = 0;
+			$scope.id = $routeParams.id;
+
+			$http({
+				method : 'GET',
+				url : 'http://192.168.0.201:8080/v2/tasks/get/' + $routeParams.id,
+				headers : {
+					'X-Auth-Token' : AccountService.getCookiesInfoToken()
+				}
+			}).then(function successCallback(response) {
+
+				if (!!response.data.result) {
+					console.log(response);
+				} else {
+					alert(response.data.error.message);
+				}
+
+			});
+
+			
 		}]);
 })();
