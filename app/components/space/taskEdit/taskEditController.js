601acf697a0669d7398d4396ab8c21d357ee7be6
yoobin <yoobin@ttadag.com>
2016-01-23 21:42:55 +0900

    집에가서 추가작업해야겟다..;

--- app/components/space/taskEdit/taskEditController.js
+++ app/components/space/taskEdit/taskEditController.js
@@ -1,8 +1,10 @@
 (function() {
 	angular.module('ngTtadagApp.spaceEditList.taskEditController')
 		.controller('taskEditController', ['$scope', '$routeParams', '$http', 'AccountService', function($scope, $routeParams, $http, AccountService) {
+			var i = 0;
 			$scope.index = 0;
 			$scope.id = $routeParams.id;
+			$scope.nodes = [];
 
 			$http({
 				method : 'GET',
@@ -13,13 +15,43 @@
 			}).then(function successCallback(response) {
 
 				if (!!response.data.result) {
-					console.log(response);
+
+					for(; i < response.data.result.taskUnits.length; i++) {
+
+						if(response.data.result.taskUnits[i].hasOwnProperty('ALARM')) {
+							$scope.nodes.push('ALARM');
+						} else if(response.data.result.taskUnits[i].hasOwnProperty('LIGHT')) {
+							$scope.nodes.push('LIGHT');
+						} else if(response.data.result.taskUnits[i].hasOwnProperty('SPEAKER')) {
+							$scope.nodes.push('SPEAKER');
+						} else if(response.data.result.taskUnits[i].hasOwnProperty('BUTTON')) {
+							$scope.nodes.push('BUTTON');
+						}
+					}
+
+					//console.log(response.data.result.taskUnits);
 				} else {
+
 					alert(response.data.error.message);
+
 				}
 
 			});
 
-			
-		}]);
+
+		}])
+		.directive('nodeSettingSection', function() {
+			return {
+				restrict: 'E',
+				//template : '<div></div>',
+				//templateUrl: 'directive.html',
+				replace: true,
+				priority: 0,
+				transclude: false,
+				scope: true,
+				controller: function($scope, $element, $attrs, $transclude, $mdBottomSheet, NetworkService, $mdToast) {
+
+				}
+			}
+		});
 })();
