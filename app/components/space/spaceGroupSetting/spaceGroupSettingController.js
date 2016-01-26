585265c592174b3eba3fff2ff63ad87fb441f6a1
yoobin <yoobin@ttadag.com>
2016-01-26 22:38:39 +0900

    그룹 이름 변경 및 그룹나가기 적용

--- app/components/space/spaceGroupSetting/spaceGroupSettingController.js
+++ app/components/space/spaceGroupSetting/spaceGroupSettingController.js
@@ -14,6 +14,32 @@
 			});
 
 
+			$scope.groupChangeName = function() {
+
+				$http({
+					method: 'POST',
+					url: 'http://192.168.0.201:8080/v2/groups/rename/' + $routeParams.id,
+					data : {
+						alias : $scope.alias
+					},
+					headers: {
+						'X-Auth-Token': AccountService.getCookiesInfoToken()
+					}
+				}).then(function successCallback(response) {
+					//$scope.alias = response.data.result.alias;
+				});
+			};
+			$scope.groupRemove = function() {
+				$http({
+					method: 'GET',
+					url: 'http://192.168.0.201:8080/v2/groups/remove/' + $routeParams.id,
+					headers: {
+						'X-Auth-Token': AccountService.getCookiesInfoToken()
+					}
+				}).then(function successCallback(response) {
+					//$scope.alias = response.data.result.alias;
+				});
+			};
 
 		}]);
 })();
