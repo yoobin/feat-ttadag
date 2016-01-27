031d5f825ad6e4da6e68188b8a51ae023b269802
yoobin <yoobin@ttadag.com>
2016-01-27 21:42:58 +0900

    selected list 뿌리기

--- app/components/space/spaceCategory/spaceCategoryListController.js
+++ app/components/space/spaceCategory/spaceCategoryListController.js
@@ -1,7 +1,23 @@
 (function() {
 	'use strict';
 	angular.module('ngTtadagApp.spaceCategoryListController.categoryListController')
-		.controller('categoryListController',['$scope', function($scope) {
-			console.log('!!!');
+		.controller('categoryListController',['$scope', '$routeParams', '$http', 'AccountService', function($scope, $routeParams, $http, AccountService) {
+			//$routeParams.type
+			//$routeParams.id
+
+			$http({
+				method : 'GET',
+				url : 'http://192.168.0.201:8080/v2/tasks/list/category/' + $routeParams.id + '/' + $routeParams.type,
+				headers : {
+					'X-Auth-Token' : AccountService.getCookiesInfoToken()
+				}
+			}).then(function successCallback(response) {
+				$scope.CategoryContent = response.data.result;
+
+
+				$scope.selectedItem = function() {
+
+				}
+			});
 		}]);
 })();
