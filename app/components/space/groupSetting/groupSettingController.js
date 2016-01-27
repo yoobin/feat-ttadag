7bc23572f5cc3a5308cc49adfcaa5af3891609bc
yoobin <yoobin@ttadag.com>
2016-01-27 20:20:17 +0900

    카탈로그 페이지 추가

--- app/components/space/groupSetting/groupSettingController.js
+++ app/components/space/groupSetting/groupSettingController.js
@@ -3,5 +3,38 @@
 	angular.module('ngTtadagApp.spaceGroupSetting.groupSettingController')
 		.controller('groupSettingController',['$scope', '$routeParams', '$location', 'AccountService', '$http', function($scope, $routeParams, $location, AccountService, $http) {
 
+			$scope.template = {};
+			//$scope.template.url = 'taskAdd.html';
+
+				$scope.tabEvent = function(type) {
+
+				type = type || 'taskAdd';
+
+				if(type === 'group') {
+
+				} else if(type === 'taskAdd') {
+
+					$http({
+						method : 'GET',
+						url : 'http://192.168.0.201:8080/v2/categorys/list',
+						headers : {
+							'X-Auth-token' :AccountService.getCookiesInfoToken()
+						}
+					}).then(function successCallback(response) {
+
+						$scope.catalogs = response.data.result
+
+					});
+
+				} else {
+
+				}
+
+				$scope.template.url = type + '.html';
+
+			};
+
+			$scope.tabEvent();
+
 		}]);
 })();
