6a3814808d3fe3119f66b1c62ab2a73aac139328
yoobin <yoobin@ttadag.com>
2016-01-27 22:22:23 +0900

    카테고리 선택부분도 완료~!!!드디어!!

--- app/components/space/spaceCategory/spaceCategoryListController.js
+++ app/components/space/spaceCategory/spaceCategoryListController.js
@@ -1,9 +1,16 @@
 (function() {
 	'use strict';
 	angular.module('ngTtadagApp.spaceCategoryListController.categoryListController')
-		.controller('categoryListController',['$scope', '$routeParams', '$http', 'AccountService', function($scope, $routeParams, $http, AccountService) {
+		.controller('categoryListController',['$scope', '$routeParams', '$http', '$location', 'AccountService', function($scope, $routeParams, $http, $location, AccountService) {
 			//$routeParams.type
 			//$routeParams.id
+			var tmpObj = {},
+				tmpArray = [];
+
+			$scope.selected = [];
+
+
+
 
 			$http({
 				method : 'GET',
@@ -15,8 +22,39 @@
 				$scope.CategoryContent = response.data.result;
 
 
-				$scope.selectedItem = function() {
+				$scope.toggle = function (item, list) {
+					var idx = list.indexOf(item);
+					if (idx > -1) list.splice(idx, 1);
+					else list.push(item);
+				};
+				$scope.exists = function (item, list) {
+					return list.indexOf(item) > -1;
+				};
 
+
+
+				$scope.selectedItemComplete = function() {
+
+					//console.log($scope.selected);
+
+					angular.forEach($scope.selected, function(id) {
+						tmpObj.id = id;
+						tmpArray.push(tmpObj);
+					});
+
+					$http({
+						method : 'POST',
+						url : 'http://192.168.0.201:8080/v2/groups/add/task/' + $routeParams.id,
+						data : {
+							id : $routeParams.id,
+							tasks : tmpArray
+						},
+						headers : {
+							'X-Auth-Token' : AccountService.getCookiesInfoToken()
+						}
+					}).then(function successCallback(response) {
+						$location.path('space/taskList/group/' + $routeParams.id);
+					});
 				}
 			});
 		}]);
