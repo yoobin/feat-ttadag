91d734f3b00cb6f81e59efc86cf3b20866682a3b
yoobin <yoobin@ttadag.com>
2016-01-28 15:24:12 +0900

    헤더 컨트롤러 분기 처리

--- app/shared/header/headerController.js
+++ app/shared/header/headerController.js
@@ -6,16 +6,36 @@
 	'use strict';
 	angular.module('header')
 		.controller('headerController', ['$scope', '$location', 'AccountService', function($scope, $location, AccountService) {
+			$scope.template = {};
+
+			$scope.$on('$routeChangeSuccess', function(next, current) {
+
+				if (($location.path() !== '/account/signIn') && ($location.path() !== '/account/signUp')) {
+
+					$scope.header01 = {'text-align': 'center','background-color':'#473B30'};
+					$scope.template.url = 'header02.html';
+
+				} else {
+
+					$scope.header01 = {'text-align': 'center','background-color':'#252032'};
+					$scope.template.url = 'header01.html';
+					$scope.headerEvent = function(type) {
+						if (type === 'signIn') {
+
+							$location.path('/account/signIn');
+
+						} else if( type === 'signUp') {
+							$location.path('/account/signUp');
+						}
+					}
+				}
+
+
+
+
+			});
 			//console.log($scope);
 			//console.log(AccountService.getCookiesInfoIsLogin());
-			$scope.headerEvent = function(type) {
-				if (type === 'signIn') {
 
-					$location.path('/account/signIn');
-
-				} else if( type === 'signUp') {
-					$location.path('/account/signUp');
-				}
-			}
 		}]);
 })();
