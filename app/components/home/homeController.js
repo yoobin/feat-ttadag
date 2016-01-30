9754c95f993b3c87bb653e31ef47bb72b30a0930
yoobin <yoobin@ttadag.com>
2016-01-30 10:21:35 +0900

    최초 로딩 수정

--- app/components/home/homeController.js
+++ app/components/home/homeController.js
@@ -6,36 +6,37 @@
 		$scope.isAutoLogin = false;
 		$scope.isWIFIAccess = false;
 		$scope.etcCheck = false;
-		$scope.go = false;
+		//$scope.go = false;
 
 		$scope.isLogin = AccountService.getCookiesInfoIsLogin();
 		$scope.start = function() {
 			$location.path('/space/groupList');
 		};
-		if(!$scope.isLogin) {
-			var loadingCount = function () {
 
-				if ($scope.count >= 1) {
-					$timeout(loadingCount, 1000);
-					$scope.count--;
-
-					if ($scope.count === 4) {
-						$scope.isAutoLogin = true;
-					} else if ($scope.count === 3) {
-						$scope.isWIFIAccess = true;
-					} else if ($scope.count === 2) {
-						$scope.etcCheck = true;
-					} else {
-						$scope.go = true;
-					}
-				} else {
-					$timeout.cancel(loadingCount);
-				}
-
-
-			};
-			$timeout(loadingCount, 1000);
-		}
+		//if(!$scope.isLogin) {
+		//	var loadingCount = function () {
+		//
+		//		if ($scope.count >= 1) {
+		//			$timeout(loadingCount, 1000);
+		//			$scope.count--;
+		//
+		//			if ($scope.count === 4) {
+		//				$scope.isAutoLogin = true;
+		//			} else if ($scope.count === 3) {
+		//				$scope.isWIFIAccess = true;
+		//			} else if ($scope.count === 2) {
+		//				$scope.etcCheck = true;
+		//			} else {
+		//				$scope.go = true;
+		//			}
+		//		} else {
+		//			$timeout.cancel(loadingCount);
+		//		}
+		//
+		//
+		//	};
+		//	$timeout(loadingCount, 1000);
+		//}
 
 
 		$scope.signOut = function() {
